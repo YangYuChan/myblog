bad1b466f8628def5a6725be380a5e01c6a75176
YangYuChan <yuchan_yang@126.com>
2018-11-20 18:09:55 +0800

    [项目重构]

---
+++ src/components/Menu.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="left_wrapper">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Menu"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+      <Menu></Menu>
+    </div>
+</template>
+
+<script>
+  import Menu from '../components/Menu'
+    export default {
+      name: "Index",
+      components:{
+        Menu,
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
